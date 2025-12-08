28b7c316e6482d80b1f08cc983744810cc5a6357
Hasanujjaman <codechef7872@gmail.com>
2025-12-08 18:50:32 +0530

    Removed uploads folder from tracking

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,23 @@
-import mongoose from "mongoose";
+/*import mongoose from "mongoose";
 
 // Connect to MongoDB using Mongoose
 export const connectDB = async () => {
   await mongoose.connect(
     'mongodb+srv://ronymondal9609:fooddel@cluster0.x3a5q9p.mongodb.net/Food-Del-App'
+  )
+  .then(() => console.log("DB Connected")); // Successful connection log
+};
+*/
+
+
+
+import mongoose from "mongoose";
+
+// Connect to MongoDB using Mongoose
+export const connectDB = async () => {
+  await mongoose.connect(
+    
+"mongodb+srv://ronymondal9609_db_user:mCSRNaOx5bGQshrX@cluster0.wqjejon.mongodb.net/Food-Delivery"
   )
   .then(() => console.log("DB Connected")); // Successful connection log
 };
